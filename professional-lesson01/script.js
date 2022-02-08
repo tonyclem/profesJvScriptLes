6d2ae200045e0627635b6c213c9f4d4c872d73e4
tonyclem <tonyclem001@gmail.com>
2022-02-08 19:39:11 +0100

    Simple page, used to practice debugger

---
+++ professional-lesson01/script.js
@@ -0,0 +1,22 @@
+// Selecting all p element
+const allParagraph = document.getElementsByTagName("p");
+
+// Declearing eventlistener on button in other to click and make it more interactive
+document.getElementsByTagName("button")[0].addEventListener("click", () => {
+  // receive the data and store it
+  let allContent = "";
+  // count start from zero
+  let count = 0;
+
+  // using for over all paragraph element
+  for (let i = 0; i < allParagraph.length; i++) {
+    // assign all the paragraph to node
+    const node = allParagraph[i];
+    // increment count
+    count++;
+    // added the count and text to allcontent \n mean break to new line
+    allContent += `${count} - ${node.textContent}\n`;
+  }
+  //   Show alert after loop with number, \n break to new line
+  alert(`Found ${count} Paragraph. Their content: \n${allContent}`);
+});
